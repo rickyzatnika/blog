1762c44adc1eac18896e3ee2cb8c244052f0b471
rickyzatnika <rickyzatnika91@gmail.com>
2022-02-22 16:10:59 +0700

    add custom page 404

---
+++ pages/404.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Custom404() {
+  return (
+    <div className='text-center lg:mt-40 md:mt-10 bg-slate-400 bg-opacity-10 rounded-full shadow-xl max-w-fit mx-auto p-20'>
+        <h4>Oppppssssss.. Maaf </h4>
+        <div className='text-6xl my-8 flex justify-center flex-col'>
+            404
+            <h2 className='text-3xl font-bold animate-bounce text-teal-500'>PAGE NOT FOUND !!</h2>
+        </div>
+        <h4 className='uppercase'>Halaman yang kamu tuju tidak ada</h4>
+    </div>
+  )
+}
