7f6ce82b81ef8e123d31b21a1af81ba8680d7c16
rickyzatnika <rickyzatnika91@gmail.com>
2022-02-23 09:37:48 +0700

    change post details color

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -93,7 +93,7 @@ const PostDetail = ({ post }) => {
         </div>
         <h1 className="mb-8 text-3xl font-semibold ">{post.title}</h1>
         
-       <article className='text-gray-500 p-0 m-0'>
+       <article className='text-neutral-700 p-0 m-0'>
         {post.content.raw.children.map((typeObj, index) => {
             const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item))
             
