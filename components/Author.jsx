e8141b644d718d2548fbbee66ebbe85320b30010
rickyzatnika <rickyzatnika91@gmail.com>
2022-02-23 13:40:41 +0700

    fixed author image

--- components/Author.jsx
+++ components/Author.jsx
@@ -7,12 +7,12 @@ const Author = ({ author }) => {
   return (
     <div className='text-center mt-20 mb-8 p-8 mx-auto w-80 relative rounded-lg shadow-lg bg-neutral-100'>
       <div className="absolute left-4  top-5 ">
-        <div className="flex items-center justify-center align-middle mr-2 border-b rounded-full">
+        <div className="flex items-center justify-center align-middle mr-2 border-b">
           <img
             alt={author.name}
             width='28px'
             heigth='28px'
-            className='animate-bounce relative align-middle '
+            className='animate-bounce relative align-middle  rounded-full'
             src={author.photo.url}
           />
           <p className='absolute left-6 right-0 text-neutral-500'>postBy_</p>
