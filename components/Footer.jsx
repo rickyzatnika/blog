4a6493e69b555aa00e34b6293a8f11d0536feaf6
rickyzatnika <rickyzatnika91@gmail.com>
2022-02-26 21:24:53 +0700

    edit footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,10 +3,14 @@ import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <div className='text-center py-6 text-white bg-black bg-opacity-90 text-md'>Allright reserved © 2022
-        <div className="text-teal-700 underline hover:text-teal-500 w-fit mx-auto">
-            <Link href='https://ryza.vercel.app/'> ryza.vercel.app</Link>
+    <div className="container bg-black bg-opacity-90 block text-center md:flex justify-around align-middle items-center max-w-full">
+      <p  className="text-teal-700 hover:text-teal-500">© 2021 - 2022<Link href='https://ryza.vercel.app/'> ryza.vercel.app</Link></p>
+        <p className='text-stone-700'>sponsor by : </p>
+        <div className="flex gap-2 justify-center">
+          <p className='text-teal-700 underline hover:text-teal-500 '> <Link href='/'>Privacy Police</Link> </p>
+          <p className='text-teal-700 underline hover:text-teal-500 '> <Link href='/'>Disclaimer</Link> </p>
         </div>
+      
     </div>
   )
 }
